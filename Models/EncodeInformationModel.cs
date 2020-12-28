d62a5c40d9ba1a395e715fede81ae7198143fede
Nevaran <nevaranreal@gmail.com>
2020-12-28 16:12:43 +0200

    Initial commit

---
+++ Models/EncodeInformationModel.cs
@@ -0,0 +1,41 @@
+﻿
+namespace FfmpegEnkoder.Models
+{
+    class EncodeInformationModel
+    {
+        private string encodePath = "";
+
+        /// <summary>
+        /// The location (folder or perhaps single file) of the video(s) that will be encoded
+        /// </summary>
+        public string EncodePath
+        {
+            get
+            {
+                return encodePath;
+            }
+            set
+            {
+                encodePath = value;
+            }
+        }
+
+        private string finishPath = "";
+
+        /// <summary>
+        /// The location (normally folder directory) where the encoded videos will be
+        /// </summary>
+        public string FinishPath
+        {
+            get
+            {
+                return finishPath;
+            }
+            set
+            {
+                finishPath = value;
+            }
+        }
+
+    }
+}
---
+++ Pages/ShellViewModel.cs
@@ -0,0 +1,27 @@
+﻿using FfmpegEnkoder.Models;
+using Stylet;
+
+namespace FfmpegEnkoder.Pages
+{
+    public class ShellViewModel : Screen
+    {
+        EncodeInformationModel EncodeInfo { get; set; } = new EncodeInformationModel();
+
+        protected override void OnInitialActivate()
+        {
+            base.OnInitialActivate();
+
+
+        }
+
+        public void OnEncodePathSet(string newPath)
+        {
+            EncodeInfo.EncodePath = newPath;
+        }
+
+        public void OnFinishPathSet(string newPath)
+        {
+            EncodeInfo.FinishPath = newPath;
+        }
+    }
+}
